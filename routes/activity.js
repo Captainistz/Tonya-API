0b9f82ccfb7014d76dc484cc80c2a04dafc41cb4
Captainistz <mynameisjeff7614@gmail.com>
2022-10-21 05:11:20 +0700

    😛 implemented activity api

--- routes/activity.js
+++ routes/activity.js
@@ -1,7 +1,16 @@
-const { db } = require('../services/firebase')
+const { db, Timestamp } = require('../services/firebase')
 
 const getAvailableActivity = async (req, res) => {
   try {
+    const _res = await db.collection('activity').where('active', '==', true).orderBy('start_time', 'asc').get()
+    if (_res.empty) {
+      return res.status(404).json({ data: 'Not found 😶' })
+    }
+    const data = {}
+    _res.forEach((e) => {
+      data[e.id] = e.data()
+    })
+    return res.status(200).json(data)
   } catch (e) {
     return res.status(500).json({ data: String(e) })
   }
@@ -9,6 +18,11 @@ const getAvailableActivity = async (req, res) => {
 
 const getActivity = async (req, res) => {
   try {
+    const _res = await db.collection('activity').doc(req.params['id']).get()
+    if (!_res) {
+      return res.status(404).json({ data: 'Not found 😶' })
+    }
+    return res.status(200).json(_res.data())
   } catch (e) {
     return res.status(500).json({ data: String(e) })
   }
@@ -16,13 +30,21 @@ const getActivity = async (req, res) => {
 
 const postActivity = async (req, res) => {
   try {
-  } catch (e) {
-    return res.status(500).json({ data: String(e) })
-  }
-}
-
-const updateActivity = async (req, res) => {
-  try {
+    const data = req.body
+    const _res = await db.collection('activity').add({
+      owner_id: data['owner_id'],
+      participant: [],
+      max_participant: data['max_participant'],
+      active: true,
+      start_time: Timestamp.fromDate(new Date(data['start_time'])),
+      end_time: Timestamp.fromDate(new Date(data['end_time'])),
+      description: data['description'],
+      tag: data['tag'],
+    })
+    if (!_res) {
+      throw 'Error occurred'
+    }
+    return res.status(200).json({ data: 'done 😁' })
   } catch (e) {
     return res.status(500).json({ data: String(e) })
   }
@@ -32,5 +54,4 @@ module.exports = {
   getAvailableActivity,
   getActivity,
   postActivity,
-  updateActivity,
 }
