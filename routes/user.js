576bb69a497b450edc6748b76b76733d2bc50515
Captainistz <mynameisjeff7614@gmail.com>
2022-10-21 05:11:00 +0700

    😜 implemented user api

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,46 @@
-const { db } = require('../services/firebase')
+const { db, FieldValue } = require('../services/firebase')
 
-const getAllUser = async (req, res) => {
+const getAllUser = async (_req, res) => {
   try {
+    const _res = await db.collection('user').get()
+    if (_res.empty) {
+      return res.status(404).json({ data: 'Not found 😶' })
+    }
+    const data = {}
+    _res.forEach((e) => {
+      data[e.id] = e.data()
+    })
+    return res.status(200).json(data)
   } catch (e) {
     return res.status(500).json({ data: String(e) })
   }
 }
 
 const getUser = async (req, res) => {
+  try {
+    const _res = await db.collection('user').doc(req.params['id']).get()
+    if (!_res) {
+      return res.status(404).json({ data: 'Not found 😶' })
+    }
+    return res.status(200).json(_res.data())
+  } catch (e) {
+    return res.status(500).json({ data: String(e) })
+  }
+}
+
+const getUserFriend = async (req, res) => {
+  try {
+    const _res = await db.collection('user').doc(req.params['id']).get()
+    if (!_res) {
+      return res.status(404).json({ data: 'Not found 😶' })
+    }
+    return res.status(200).json({ friend: _res.data()['friend'] })
+  } catch (e) {
+    return res.status(500).json({ data: String(e) })
+  }
+}
+
+const getUserActivity = async (req, res) => {
   try {
   } catch (e) {
     return res.status(500).json({ data: String(e) })
@@ -16,6 +49,62 @@ const getUser = async (req, res) => {
 
 const postUser = async (req, res) => {
   try {
+    const check = await db.collection('user').doc(req.body['uid']).get()
+    if (check.data()) {
+      return res.status(200).json({ data: 'existed 😁' })
+    }
+    const _res = await db.collection('user').doc(req.body['uid']).set(
+      {
+        name: req.body['name'],
+        email: req.body['email'],
+        tel: req.body['tel'],
+        friend: [],
+      },
+      { merge: 1 }
+    )
+    if (!_res) {
+      throw 'Error occurred'
+    }
+    return res.status(200).json({ data: 'done 😁' })
+  } catch (e) {
+    return res.status(500).json({ data: String(e) })
+  }
+}
+
+const postUserFriend = async (req, res) => {
+  try {
+    const _res = await db
+      .collection('user')
+      .doc(req.params['id'])
+      .update(
+        {
+          friend: FieldValue.arrayUnion(req.body['uid']),
+        },
+        { merge: 1 }
+      )
+    if (!_res) {
+      throw 'Error occurred'
+    }
+    return res.status(200).json({ data: 'done 😁' })
+  } catch (e) {
+    return res.status(500).json({ data: String(e) })
+  }
+}
+const deleteUserFriend = async (req, res) => {
+  try {
+    const _res = await db
+      .collection('user')
+      .doc(req.params['id'])
+      .update(
+        {
+          friend: FieldValue.arrayRemove(req.body['uid']),
+        },
+        { merge: 1 }
+      )
+    if (!_res) {
+      throw 'Error occurred'
+    }
+    return res.status(200).json({ data: 'done 😁' })
   } catch (e) {
     return res.status(500).json({ data: String(e) })
   }
@@ -24,5 +113,9 @@ const postUser = async (req, res) => {
 module.exports = {
   getAllUser,
   getUser,
+  getUserFriend,
+  getUserActivity,
   postUser,
+  postUserFriend,
+  deleteUserFriend,
 }
