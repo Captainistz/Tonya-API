f02bf90d3d96deb6669820a90586f8c2602cc5fc
Captainistz <mynameisjeff7614@gmail.com>
2022-10-21 05:12:20 +0700

    😘 recoded

--- app.js
+++ app.js
@@ -26,24 +26,22 @@ app.get('/', (_req, res) => {
 
 //user
 app.get('/user', require('./routes/user').getAllUser)
+app.post('/user', require('./routes/user').postUser)
+
 app.get('/user/:id', require('./routes/user').getUser)
 
-app.post('/user', require('./routes/user').postUser)
+app.get('/user/:id/friend', require('./routes/user').getUserFriend)
+app.post('/user/:id/friend', require('./routes/user').postUserFriend)
+app.delete('/user/:id/friend', require('./routes/user').deleteUserFriend)
+
+app.get('/user/:id/activity', require('./routes/user').getUserActivity)
 
 //activity
 app.get('/activity', require('./routes/activity').getAvailableActivity)
-app.get('/activity/:id', require('./routes/activity').getActivity)
-
 app.post('/activity', require('./routes/activity').postActivity)
 
-app.put('/activity/:id', require('./routes/activity').updateActivity)
+app.get('/activity/:id', require('./routes/activity').getActivity)
 
-//friend
-app.get('/friend/:id', require('./routes/friend').getFriend)
-
-app.post('/friend/:id', require('./routes/friend').postFriend)
-
-app.delete('/friend/:id', require('./routes/friend').deleteFriend)
 
 const PORT = process.env.PORT || 5000
 
